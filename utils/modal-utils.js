a5d8a48d2413abd49ebe177f928a3978227d7d15
alinaduca <alina_duca@yahoo.com>
2023-09-05 08:40:36 +0300

    Modif at modals

--- utils/modal-utils.js
+++ utils/modal-utils.js
@@ -32,6 +32,7 @@ export function closeModal(element) {
     }
 }
 
+
 export async function showActionBox(targetElement, primaryKey, componentName, insertionMode) {
     const existingComponentNode = document.getElementById(`${primaryKey}`);
     if (existingComponentNode) {
