1c69b0032561dae1550fbfcfc2f95764eb431b70
alupoaiemircea <mircea@axiologic.net>
2023-09-27 17:17:39 +0300

    in progress authentication using presenters

--- utils/authentication-utils.js
+++
@@ -1,9 +0,0 @@
-export function addUserToLocalStorage(user) {
-    let usersString = localStorage.getItem("users");
-    let users = [];
-    if(usersString) {
-        users = JSON.parse(usersString);
-    }
-    users.push(user);
-    localStorage.setItem("users", JSON.stringify(users));
-}
