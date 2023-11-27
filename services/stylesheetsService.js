8e4fd780b4424fa1ed111ff438cf64ab5c4034fe
Koroem <conduratvictor@yahoo.com>
2023-11-27 13:58:01 +0200

    Replaced SET with MAP to fix the issue where the same component would be reinserted in the HTML, and would not be styled

--- services/stylesheetsService.js
+++ services/stylesheetsService.js
@@ -1,37 +1,41 @@
 export class StylesheetsService {
     constructor() {
-        this.loadedStyleSheets = new Set();
+        this.loadedStyleSheets = new Map();
     }
+
     async loadStyleSheet(href) {
-        if (!this.loadedStyleSheets.has(href)) {
+        let refCount = this.loadedStyleSheets.get(href) || 0;
+        if (refCount === 0) {
             await new Promise((resolve, reject) => {
                 const link = document.createElement('link');
                 link.rel = 'stylesheet';
                 link.href = href;
-                link.onload = () => {
-                    this.loadedStyleSheets.add(href);
-                    resolve();
-                };
+                link.onload = () => resolve();
                 link.onerror = () => reject(new Error(`Failed to load the CSS file: ${href}`));
                 document.head.appendChild(link);
             });
         }
+        this.loadedStyleSheets.set(href, refCount + 1);
     }
+
     unloadStyleSheet(href) {
-        if (this.loadedStyleSheets.has(href)) {
+        let refCount = this.loadedStyleSheets.get(href) || 0;
+        if (refCount > 1) {
+            this.loadedStyleSheets.set(href, refCount - 1);
+        } else if (refCount === 1) {
             const link = document.head.querySelector(`link[href="${href}"]`);
             if (link) {
                 document.head.removeChild(link);
-                this.loadedStyleSheets.delete(href);
             }
+            this.loadedStyleSheets.delete(href);
         }
     }
 
     async loadStyleSheets(hrefArray) {
         await Promise.all(hrefArray.map(href => this.loadStyleSheet(href)));
     }
-    async unloadStyleSheets(hrefArray) {
-        await Promise.all(hrefArray.map(href => this.unloadStyleSheet(href)));
 
+    async unloadStyleSheets(hrefArray) {
+        hrefArray.forEach(href => this.unloadStyleSheet(href));
     }
 }
