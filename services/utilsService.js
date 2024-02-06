c839acbb4773e15a8ba23d892f1a2c00cb5a07f6
Koroem <conduratvictor@yahoo.com>
2024-02-06 23:11:19 +0200

    Function to get the current env of the user: browser and version

--- services/utilsService.js
+++ services/utilsService.js
@@ -1,14 +1,17 @@
 export class UtilsService {
-    constructor() {}
+    constructor() {
+    }
+
     async initialize() {
         const utilModules = [
-            { key: 'domUtils', path: '../utils/dom-utils.js' },
-            { key: 'formUtils', path: '../utils/form-utils.js' },
-            { key: 'modalUtils', path: '../utils/modal-utils.js' },
-            { key: 'templateUtils', path: '../utils/template-utils.js' }
+            {key: 'domUtils', path: '../utils/dom-utils.js'},
+            {key: 'formUtils', path: '../utils/form-utils.js'},
+            {key: 'modalUtils', path: '../utils/modal-utils.js'},
+            {key: 'templateUtils', path: '../utils/template-utils.js'},
+            {key: 'browserUtils', path: '../utils/browser-utils.js'}
         ];
 
-        for (const { key, path } of utilModules) {
+        for (const {key, path} of utilModules) {
             const moduleExports = await import(path);
             for (const [exportName, exportFunction] of Object.entries(moduleExports)) {
                 this[exportName] = exportFunction;
